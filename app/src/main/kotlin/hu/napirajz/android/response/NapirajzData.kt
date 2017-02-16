bf7a425063b70a5f5a5242492175f387fc01c002
Gergely Topolyai <gerely01@gmail.com>
2017-02-16 09:05:05 +0100

    date

--- app/src/main/kotlin/hu/napirajz/android/response/NapirajzData.kt
+++ app/src/main/kotlin/hu/napirajz/android/response/NapirajzData.kt
@@ -2,14 +2,15 @@ package hu.napirajz.android.response
 
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.*
 
 data class NapirajzData(
         @SerializedName("ID")
         val id: String,
         @SerializedName("Cim")
         val cim: String,
-        //    @SerializedName("Datum")
-        //    private Date datum;
+        @SerializedName("Datum")
+        val datum: Date,
         @SerializedName("URL")
         val url: String,
         @SerializedName("LapURL")
